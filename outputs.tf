62de316e705a30970528cb5315a0aa65db2b1e09
ntno <ntno@users.noreply.github.com>
2022-12-26 11:52:04 -0500

    add arn to output (#3)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "cd_role_name" {
   description = "CD IAM Role Name"
   value       = aws_iam_role.cd_role.name
 }
+
+output "ci_role_arn" {
+  description = "CI IAM Role ARN"
+  value       = aws_iam_role.ci_role.arn
+}
+
+output "cd_role_arn" {
+  description = "CD IAM Role ARN"
+  value       = aws_iam_role.cd_role.arn
+}
