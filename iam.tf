4ae24f716016e6f4e9b921f9acb45cbd0451e401
ntno <ntno@users.noreply.github.com>
2022-12-26 00:20:14 -0500

    wip

--- iam.tf
+++ iam.tf
@@ -12,15 +12,17 @@ resource "aws_iam_policy" "read_write_artifacts_bucket_cicd_policy" {
 }
 
 resource "aws_iam_policy" "ssm_ci_policy" {
+  count       = local.enable_ci_ssm_policy ? 1 : 0
   name        = format("CI_ReadWrite_SSM_%s", var.site_bucket)
   path        = "/CustomerManaged/"
   description = format("Allows read/write on %s CI SSM Parameters", var.site_bucket)
   tags        = var.tags
 
-  policy = module.ssm_ci_policy_document.policy_json
+  policy = module.ssm_ci_policy_document[1].policy_json
 }
 
 module "ssm_ci_policy_document" {
+  count       = local.enable_ci_ssm_policy ? 1 : 0
   source = "./modules/dynamic-ssm-policy"
   read   = var.ci_ssm_paths["read"]
   write  = var.ci_ssm_paths["write"]
