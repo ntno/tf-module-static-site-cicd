0a0bc6bdcea2908a9d6dc0fb88f248d0b5fefa30
ntno <ntno@users.noreply.github.com>
2022-12-27 16:29:16 -0500

    refactor outputs

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,25 @@
-output "artifacts_bucket_name" {
-  description = "S3 artifacts bucket name"
-  value       = aws_s3_bucket.artifacts_bucket.id
-}
-
-output "ci_role" {
-  description = "CI IAM Role Name and ARN"
+output "artifacts_bucket_info" {
+  description = "Map containing the artifacts bucket's arn and id"
   value = {
-    iam_role_name = module.ci_role.role_name
-    iam_role_arn  = module.ci_role.role_arn
+    arn = aws_s3_bucket.artifacts_bucket.arn
+    id  = aws_s3_bucket.artifacts_bucket.id
   }
 }
 
-output "cd_roles" {
-  description = "CD IAM Role name/ARN/github_environment_name"
+output "ci_role_info" {
+  description = "Map containing the CI IAM Role's arn and name"
+  value = {
+    arn  = module.ci_role.role_arn
+    name = module.ci_role.role_name
+  }
+}
+
+output "cd_role_info" {
+  description = "environment name to Map containing CD IAM Role's arn, name, and associated github_environment_name"
   value = {
     for key, val in module.cd_roles : key => {
-      iam_role_name           = val.role_name
-      iam_role_arn            = val.role_arn
+      arn                     = val.role_arn
+      name                    = val.role_name
       github_environment_name = val.github_environment_name
     }
   }
