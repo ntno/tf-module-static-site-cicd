2433ffad1c0e7413544776352f91bc115dc82f42
ntno <ntno@users.noreply.github.com>
2022-12-27 16:00:54 -0500

    wip - map output

--- outputs.tf
+++ outputs.tf
@@ -8,5 +8,18 @@ output "ci_role" {
 
 output "cd_roles" {
   description = "list of CD IAM Role name/ARN/github_environment_name"
-  value       = module.cd_roles[*].role_arn
+  value = {
+    for key, val in module.cd_roles : key => {
+      iam_role_name = val.role_name
+      iam_role_arn  = val.role_arn
+    }
+  }
+
+  #  {for key,val in var.my_network : key => val if val.name != "" && val.cidr != ""}
+  # value       = tomap(
+  #   for e in module.cd_roles : e.github_environment_name => {
+  #     iam_role_name = e.role_name
+  #     iam_role_arn = e.role_arn
+  #   }
+  # )
 }
