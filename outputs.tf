15c555bb88122e9bce89ebe6f90381881c00c0fa
ntno <ntno@users.noreply.github.com>
2022-12-27 15:13:01 -0500

    troubleshooting

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,12 @@
 output "ci_role" {
   description = "CI IAM Role Name and ARN"
-  value       = module.ci_role.output
+  value       = {
+    iam_role_name = module.ci_role.role_name
+    iam_role_arn = module.ci_role.role_arn
+  }
 }
 
 output "cd_roles" {
   description = "list of CD IAM Role name/ARN/github_environment_name"
-  value       = module.cd_roles[*].output
+  value       = module.cd_roles[*].role_arn
 }
