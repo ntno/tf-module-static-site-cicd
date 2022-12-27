f20a7462788b954a9bdff806e01f99b8ac396537
ntno <ntno@users.noreply.github.com>
2022-12-27 15:40:32 -0500

    can() wip

--- modules/ci-role/main.tf
+++ modules/ci-role/main.tf
@@ -3,8 +3,8 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  read_path_count   = can(var.ssm_read_paths) ? length(var.ssm_read_paths) : 0
-  write_path_count  = can(var.ssm_write_paths) ? length(var.ssm_write_paths) : 0
+  read_path_count   = can(length(var.ssm_read_paths)) ? length(var.ssm_read_paths) : 0
+  write_path_count  = can(length(var.ssm_write_paths)) ? length(var.ssm_write_paths) : 0
   enable_ssm_policy = local.read_path_count == 0 && local.write_path_count == 0 ? false : true
   iam_descriptor    = format("%s_%s", var.github_org, var.github_repo)
   iam_policy_path   = format("/CustomerManaged/%s_%s/", var.github_org, var.github_repo)
