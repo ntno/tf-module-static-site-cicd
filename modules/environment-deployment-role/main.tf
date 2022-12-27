33484c29d80df81403d51efe8ac014065d8a8ba6
ntno <ntno@users.noreply.github.com>
2022-12-27 14:25:23 -0500

    update iam path

--- modules/environment-deployment-role/main.tf
+++ modules/environment-deployment-role/main.tf
@@ -6,5 +6,5 @@ locals {
   enable_ssm_policy                  = length(var.ssm_read_paths) == 0 && length(var.ssm_write_paths) == 0 ? false : true
   enable_cloudfront_policy           = length(var.cloudfront_distribution_id) == 0 ? false : true
   iam_descriptor                     = format("%s_%s_%s", var.github_org, var.github_repo, var.github_environment_name)
-  iam_policy_path                    = format("/CustomerManaged/%s/", local.iam_descriptor)
+  iam_policy_path                    = format("/CustomerManaged/%s_%s/%s/", var.github_org, var.github_repo, var.github_environment_name)
 }
