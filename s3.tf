99dec7144400f652832e9ebe30f36fa66346755d
ntno <ntno@users.noreply.github.com>
2022-12-28 15:47:13 -0500

    prevent public objects in artifact bucket

--- s3.tf
+++ s3.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket_acl" "artifacts_bucket_acl" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "artifacts_bucket_block_public_access" {
+  bucket = aws_s3_bucket.artifacts_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "artifact_bucket_server_side_encryption" {
   bucket = aws_s3_bucket.artifacts_bucket.id
 
